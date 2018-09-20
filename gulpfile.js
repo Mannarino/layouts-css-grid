493918967b6709d620e81e602101829c8d59f66a
moises <frontend.moises@gmail.com>
2018-09-20 01:38:22 -0300

    uno

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp= require('gulp');
+const sass= require('gulp-sass');
+const autoprefixer= require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('pepe',()=>
+	gulp.src('src/styles.scss')
+	.pipe(sass())
+	
+	.pipe(gulp.dest('dist'))
+	.pipe(browserSync.stream())
+	);
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+gulp.task('water',function(){
+	gulp.watch('src/partiales/*.scss',['pepe']);
+	gulp.watch("./dist/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default',['water','browser-sync','pepe']);
